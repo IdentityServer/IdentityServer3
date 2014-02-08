dc968dd08812f422f278714b458079cde3149699
Dominick Baier <dbaier@leastprivilege.com>
2014-02-08 10:03:39 +0100

    Added prototype of To/From JWT for SignInMessage

--- source/Core/Authentication/SignInMessage.cs
+++ source/Core/Authentication/SignInMessage.cs
@@ -1,4 +1,11 @@
-﻿namespace Thinktecture.IdentityServer.Core.Authentication
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Protocols.WSTrust;
+using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.ServiceModel.Security.Tokens;
+using Thinktecture.IdentityModel.Tokens;
+namespace Thinktecture.IdentityServer.Core.Authentication
 {
     public class SignInMessage
     {
@@ -11,5 +18,45 @@
 
         public string ReturnUrl { get; set; }
         public string LoginPromptCorrelationId { get; set; }
+
+        public string ToJwt(string issuer, string audience, string key, int ttl)
+        {
+            var claims = new List<Claim>();
+            if (ReturnUrl.IsPresent())
+            {
+                claims.Add(new Claim("returnUrl", ReturnUrl));
+            }
+
+            var token = new JwtSecurityToken(
+                issuer,
+                audience,
+                claims,
+                new Lifetime(DateTime.UtcNow, DateTime.UtcNow.AddSeconds(ttl)),
+                new HmacSigningCredentials(key));
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        public static SignInMessage FromJwt(string jwt, string issuer, string audience, string key)
+        {
+            var message = new SignInMessage();
+            var handler = new JwtSecurityTokenHandler();
+            var parameters = new TokenValidationParameters
+            {
+                AllowedAudience = audience,
+                ValidIssuer = issuer,
+                SigningToken = new BinarySecretSecurityToken(Convert.FromBase64String(key))
+            };
+
+            var principal = handler.ValidateToken(jwt, parameters);
+
+            var claim = principal.FindFirst("returnUrl");
+            if (claim != null)
+            {
+                message.ReturnUrl = claim.Value;
+            }
+
+            return message;
+        }
     }
 }
