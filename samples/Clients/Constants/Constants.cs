67ee585ccb3e281dae1c42eef5868d6caed3689e
Dominick Baier <dbaier@leastprivilege.com>
2014-03-01 10:00:11 +0100

    Started clients sample project

---
+++ samples/Clients/Constants/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sample
+{
+    public static class Constants
+    {
+        public const string AuthorizeEndpoint = "http://localhost:3333/core/connect/authorize";
+        public const string TokenEndpoint = "http://localhost:3333/core/connect/token";
+    }
+}
---
+++ samples/Clients/WpfClient/LoginWebView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Navigation;
+using Thinktecture.IdentityModel.Client;
+
+namespace Thinktecture.Samples
+{
+    public partial class LoginWebView : Window
+    {
+        public AuthorizeResponse AuthorizeResponse { get; set; }
+        public event EventHandler<AuthorizeResponse> Done;
+
+        Uri _callbackUri;
+
+        public LoginWebView()
+        {
+            InitializeComponent();
+            webView.Navigating += webView_Navigating;
+
+            Closing += LoginWebView_Closing;
+        }
+
+        void LoginWebView_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            e.Cancel = true;
+            this.Visibility = Visibility.Hidden;
+        }
+
+        public void Start(Uri startUri, Uri callbackUri)
+        {
+            _callbackUri = callbackUri;
+            webView.Navigate(startUri);
+        }
+
+        private void webView_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            if (e.Uri.ToString().StartsWith(_callbackUri.AbsoluteUri))
+            {
+                AuthorizeResponse = new AuthorizeResponse(e.Uri.AbsoluteUri);
+
+                e.Cancel = true;
+                this.Visibility = System.Windows.Visibility.Hidden;
+                
+                if (Done != null)
+                {
+                    Done.Invoke(this, AuthorizeResponse);
+                }
+            }
+        }
+    }
+}
