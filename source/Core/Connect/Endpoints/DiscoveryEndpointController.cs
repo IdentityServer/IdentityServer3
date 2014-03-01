33c3ce91ba71290cb859cac2025ac00898504ba1
Dominick Baier <dbaier@leastprivilege.com>
2014-03-01 17:57:32 +0100

    Changed discovery endpoint to use dynamic

--- source/Core/Connect/Endpoints/DiscoveryEndpointController.cs
+++ source/Core/Connect/Endpoints/DiscoveryEndpointController.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Thinktecture.IdentityModel;
 using Thinktecture.IdentityServer.Core.Services;
+using Thinktecture.IdentityServer.Core.Extensions;
 
 namespace Thinktecture.IdentityServer.Core.Connect
 {
@@ -19,32 +20,24 @@ namespace Thinktecture.IdentityServer.Core.Connect
         }
 
         [Route("openid-configuration")]
-        public Dictionary<string, object> GetConfiguration()
+        public dynamic GetConfiguration()
         {
-            var doc = new Dictionary<string, object>();
+            var baseUrl = Request.GetBaseUrl(_settings.GetPublicHost());
 
-            string host = _settings.GetPublicHost();
-            if (host.IsMissing())
+            return new
             {
-                host = "https://" + Request.Headers.Host;
-            }
-
-            var baseUrl = new Uri(new Uri(host), Request.GetRequestContext().VirtualPathRoot).AbsoluteUri;
-            if (!baseUrl.EndsWith("/")) baseUrl += "/";
-
-            doc.Add("issuer", _settings.GetIssuerUri());
-            doc.Add("jwks_uri", baseUrl + ".well-known/jwks");
-            doc.Add("authorization_endpoint", baseUrl + "connect/authorize");
-            doc.Add("token_endpoint", baseUrl + "connect/token");
-            doc.Add("userinfo_endpoint", baseUrl + "connect/userinfo");
-            doc.Add("scopes_supported", _settings.GetScopes().Select(s => s.Name));
-            doc.Add("response_types_supported", Constants.SupportedResponseTypes);
-            doc.Add("response_modes_supported", Constants.SupportedResponseModes);
-            doc.Add("grant_types_supported", Constants.SupportedGrantTypes);
-            doc.Add("subject_types_support", new string[] { "pairwise", "public" });
-            doc.Add("id_token_signing_alg_values_supported", "RS256");
-
-            return doc;
+                issuer = _settings.GetIssuerUri(),
+                jwks_uri = baseUrl + ".well-known/jwks",
+                authorization_endpoint = baseUrl + "connect/authorize",
+                token_endpoint = baseUrl + "connect/token",
+                userinfo_endpoint = baseUrl + "connect/userinfo",
+                scopes_supported = _settings.GetScopes().Select(s => s.Name),
+                response_types_supported = Constants.SupportedResponseTypes,
+                response_modes_supported = Constants.SupportedResponseModes,
+                grant_types_supported = Constants.SupportedGrantTypes,
+                subject_types_support = new string[] { "pairwise", "public" },
+                id_token_signing_alg_values_supported = "RS256"
+            };
         }
 
         [Route("jwks")]
@@ -56,7 +49,7 @@ namespace Thinktecture.IdentityServer.Core.Connect
 
             var key = new
             {
-                kty = "foo",
+                kty = "RSA",
                 use = "sig",
                 kid = thumbprint,
                 x5c = new string[] { cert64 }
