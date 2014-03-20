bb11f9018c307ea5c5c6bb0b6f98d7739f51f06e
Dominick Baier <dbaier@leastprivilege.com>
2014-03-20 17:32:55 +0100

    Cleaned up config code

--- source/Core/Configuration/WebApiConfig.cs
+++ source/Core/Configuration/WebApiConfig.cs
@@ -1,13 +1,5 @@
-﻿using Autofac;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Threading;
-using System.Threading.Tasks;
+﻿using System.Net.Http.Formatting;
 using System.Web.Http;
-using System.Web.Http.Dependencies;
-using System.Web.Http.Hosting;
 
 namespace Thinktecture.IdentityServer.Core.Configuration
 {
@@ -20,53 +12,10 @@ namespace Thinktecture.IdentityServer.Core.Configuration
             config.SuppressDefaultHostAuthentication();
             config.MessageHandlers.Insert(0, new KatanaDependencyResolver());
 
-            var appXmlType = config.Formatters.XmlFormatter.SupportedMediaTypes.FirstOrDefault(t => t.MediaType == "application/xml");
-            config.Formatters.XmlFormatter.SupportedMediaTypes.Remove(appXmlType);
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
 
             return config;
         }
     }
-
-    public class KatanaDependencyResolver : System.Net.Http.DelegatingHandler
-    {
-        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
-        {
-            var owin = request.GetOwinContext();
-            var scope = owin.Get<ILifetimeScope>("idsrv:AutofacScope");
-            if (scope != null)
-            {
-                request.Properties[HttpPropertyKeys.DependencyScope] = new AutofacScope(scope);
-            }
-
-            return base.SendAsync(request, cancellationToken);
-        }
-    }
-
-    public class AutofacScope : IDependencyScope
-    {
-        ILifetimeScope scope;
-        public AutofacScope(ILifetimeScope scope)
-        {
-            this.scope = scope;
-        }
-        public object GetService(System.Type serviceType)
-        {
-            return scope.ResolveOptional(serviceType);
-        }
-
-        public System.Collections.Generic.IEnumerable<object> GetServices(System.Type serviceType)
-        {
-            if (!scope.IsRegistered(serviceType))
-            {
-                return Enumerable.Empty<object>();
-            }
-            Type type = typeof(IEnumerable<>).MakeGenericType(new Type[] { serviceType });
-            return (IEnumerable<object>)scope.Resolve(type);
-        }
-
-        public void Dispose()
-        {
-        }
-    }
-
 }
