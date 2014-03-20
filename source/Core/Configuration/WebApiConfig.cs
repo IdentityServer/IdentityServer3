ed7219695838a6a18c2712ce884fe2cb35bd6bf7
Dominick Baier <dbaier@leastprivilege.com>
2014-03-20 17:44:31 +0100

    Added ExceptionFilter (todo)

--- source/Core/Configuration/WebApiConfig.cs
+++ source/Core/Configuration/WebApiConfig.cs
@@ -1,5 +1,7 @@
 ﻿using System.Net.Http.Formatting;
 using System.Web.Http;
+using Thinktecture.IdentityServer.Core.Plumbing;
+using Thinktecture.IdentityServer.Core.Services;
 
 namespace Thinktecture.IdentityServer.Core.Configuration
 {
@@ -8,12 +10,18 @@ namespace Thinktecture.IdentityServer.Core.Configuration
         public static HttpConfiguration Configure(IdentityServerCoreOptions options)
         {
             var config = new HttpConfiguration();
+            
             config.MapHttpAttributeRoutes();
             config.SuppressDefaultHostAuthentication();
+            
             config.MessageHandlers.Insert(0, new KatanaDependencyResolver());
 
             config.Formatters.Clear();
             config.Formatters.Add(new JsonMediaTypeFormatter());
+
+            // todo
+            //var logger = config.DependencyResolver.GetService(typeof(ILogger)) as ILogger;
+            //config.Filters.Add(new ExceptionFilter(logger));
 
             return config;
         }
---
+++ source/Core/Plumbing/ExceptionFilter.cs
@@ -0,0 +1,29 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+using Thinktecture.IdentityServer.Core.Services;
+
+namespace Thinktecture.IdentityServer.Core.Plumbing
+{
+    class ExceptionFilter : IExceptionFilter
+    {
+        private readonly ILogger _logger;
+
+        public ExceptionFilter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public Task ExecuteExceptionFilterAsync(HttpActionExecutedContext actionExecutedContext, CancellationToken cancellationToken)
+        {
+            _logger.ErrorFormat("Unhandled exception: {0}", actionExecutedContext.Exception);
+
+            return Task.FromResult<object>(null);
+        }
+
+        public bool AllowMultiple
+        {
+            get { return true; }
+        }
+    }
+}
