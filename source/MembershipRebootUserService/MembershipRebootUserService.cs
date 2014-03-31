886667da669a5972ce9ae7d65e78c490e4c2c89e
Brock Allen <brockallen@gmail.com>
2014-03-31 15:50:34 -0400

    move claim map out to be shared

--- source/MembershipRebootUserService/MembershipRebootUserService.cs
+++ source/MembershipRebootUserService/MembershipRebootUserService.cs
@@ -19,7 +19,7 @@ namespace MembershipReboot.IdentityServer
 {
     public class MembershipRebootUserService : IUserService, IDisposable
     {
-        UserAccountService userAccountService;
+        protected readonly UserAccountService userAccountService;
         IDisposable cleanup;
         public MembershipRebootUserService(UserAccountService userAccountService, IDisposable cleanup)
         {
@@ -29,7 +29,7 @@ namespace MembershipReboot.IdentityServer
             this.cleanup = cleanup;
         }
 
-        public void Dispose()
+        public virtual void Dispose()
         {
             if (this.cleanup != null)
             {
@@ -38,7 +38,7 @@ namespace MembershipReboot.IdentityServer
             }
         }
 
-        public async Task<IEnumerable<System.Security.Claims.Claim>> GetProfileDataAsync(string subject,
+        public virtual async Task<IEnumerable<System.Security.Claims.Claim>> GetProfileDataAsync(string subject,
             IEnumerable<string> requestedClaimTypes = null)
         {
             var acct = userAccountService.GetByID(subject.ToGuid());
@@ -55,7 +55,7 @@ namespace MembershipReboot.IdentityServer
             return claims;
         }
 
-        IEnumerable<Claim> GetClaimsFromAccount(UserAccount account)
+        protected virtual IEnumerable<Claim> GetClaimsFromAccount(UserAccount account)
         {
             var claims = new List<Claim>{
                 new Claim(Constants.ClaimTypes.Subject, account.ID.ToString("D")),
@@ -81,7 +81,7 @@ namespace MembershipReboot.IdentityServer
             return claims;
         }
         
-        public async Task<AuthenticateResult> AuthenticateLocalAsync(string username, string password)
+        public virtual async Task<AuthenticateResult> AuthenticateLocalAsync(string username, string password)
         {
             UserAccount account;
             if (userAccountService.Authenticate(username, password, out account))
@@ -121,7 +121,7 @@ namespace MembershipReboot.IdentityServer
             return null;
         }
 
-        public async Task<ExternalAuthenticateResult> AuthenticateExternalAsync(string subject, IEnumerable<Claim> externalClaims)
+        public virtual async Task<ExternalAuthenticateResult> AuthenticateExternalAsync(string subject, IEnumerable<Claim> externalClaims)
         {
             if (externalClaims == null || !externalClaims.Any())
             {
@@ -189,15 +189,15 @@ namespace MembershipReboot.IdentityServer
             return await UpdateAccountFromExternalClaimsAsync(accountID, provider, providerId, claims);
         }
 
-        protected async Task<ExternalAuthenticateResult> SignInFromExternalProviderAsync(Guid accountID, string provider)
+        protected virtual async Task<ExternalAuthenticateResult> SignInFromExternalProviderAsync(Guid accountID, string provider)
         {
             var acct = userAccountService.GetByID(accountID);
             return new ExternalAuthenticateResult(provider, accountID.ToString("D"), GetNameForAccount(accountID));
         }
 
-        private async Task<ExternalAuthenticateResult> ProcessExistingExternalAccountAsync(Guid accountID, string provider, string providerId, IEnumerable<Claim> claims)
+        protected virtual async Task<ExternalAuthenticateResult> ProcessExistingExternalAccountAsync(Guid accountID, string provider, string providerId, IEnumerable<Claim> claims)
         {
-            claims = FilterExternalClaimsForExistingAccount(provider, claims); 
+            claims = FilterExternalClaimsForExistingAccount(accountID, provider, claims); 
 
             SetAccountEmail(accountID, ref claims);
             SetAccountPhone(accountID, ref claims);
@@ -215,13 +215,13 @@ namespace MembershipReboot.IdentityServer
 
             return null;
         }
-        
+
         protected virtual IEnumerable<Claim> FilterExternalClaimsForNewAccount(string provider, IEnumerable<Claim> claims)
         {
             return claims;
         }
 
-        protected virtual IEnumerable<Claim> FilterExternalClaimsForExistingAccount(string provider, IEnumerable<Claim> claims)
+        protected virtual IEnumerable<Claim> FilterExternalClaimsForExistingAccount(Guid accountID, string provider, IEnumerable<Claim> claims)
         {
             return claims;
         }
@@ -360,119 +360,9 @@ namespace MembershipReboot.IdentityServer
             return name;
         }
 
-        static Dictionary<string, string> ClaimTypeMap = BuildClaimTypeMap();
-
-        static Dictionary<string, string> BuildClaimTypeMap()
-        {
-            Dictionary<string, string> dictionary = new Dictionary<string, string>();
-            dictionary.Add("actort", "http://schemas.xmlsoap.org/ws/2009/09/identity/claims/actor");
-            dictionary.Add("birthdate", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/dateofbirth");
-            dictionary.Add("email", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress");
-            dictionary.Add("family_name", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/surname");
-            dictionary.Add("gender", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/gender");
-            dictionary.Add("given_name", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname");
-            dictionary.Add("sub", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
-            dictionary.Add("nameid", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier");
-            dictionary.Add("website", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/webpage");
-            // changed from unique_name
-            dictionary.Add("name", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name");
-            dictionary.Add("oid", "http://schemas.microsoft.com/identity/claims/objectidentifier");
-            dictionary.Add("scp", "http://schemas.microsoft.com/identity/claims/scope");
-            dictionary.Add("tid", "http://schemas.microsoft.com/identity/claims/tenantid");
-            dictionary.Add("acr", "http://schemas.microsoft.com/claims/authnclassreference");
-            dictionary.Add("adfs1email", "http://schemas.xmlsoap.org/claims/EmailAddress");
-            dictionary.Add("adfs1upn", "http://schemas.xmlsoap.org/claims/UPN");
-            dictionary.Add("amr", "http://schemas.microsoft.com/claims/authnmethodsreferences");
-            dictionary.Add("auth_time", "http://schemas.microsoft.com/ws/2008/06/identity/claims/authenticationinstant");
-            dictionary.Add("authmethod", "http://schemas.microsoft.com/ws/2008/06/identity/claims/authenticationmethod");
-            dictionary.Add("certapppolicy", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/applicationpolicy");
-            dictionary.Add("certauthoritykeyidentifier", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/authoritykeyidentifier");
-            dictionary.Add("certbasicconstraints", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/basicconstraints");
-            dictionary.Add("certeku", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/eku");
-            dictionary.Add("certissuer", "http://schemas.microsoft.com/2012/12/certificatecontext/field/issuer");
-            dictionary.Add("certissuername", "http://schemas.microsoft.com/2012/12/certificatecontext/field/issuername");
-            dictionary.Add("certkeyusage", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/keyusage");
-            dictionary.Add("certnotafter", "http://schemas.microsoft.com/2012/12/certificatecontext/field/notafter");
-            dictionary.Add("certnotbefore", "http://schemas.microsoft.com/2012/12/certificatecontext/field/notbefore");
-            dictionary.Add("certpolicy", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/certificatepolicy");
-            dictionary.Add("certpublickey", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/rsa");
-            dictionary.Add("certrawdata", "http://schemas.microsoft.com/2012/12/certificatecontext/field/rawdata");
-            dictionary.Add("certserialnumber", "http://schemas.microsoft.com/ws/2008/06/identity/claims/serialnumber");
-            dictionary.Add("certsignaturealgorithm", "http://schemas.microsoft.com/2012/12/certificatecontext/field/signaturealgorithm");
-            dictionary.Add("certsubject", "http://schemas.microsoft.com/2012/12/certificatecontext/field/subject");
-            dictionary.Add("certsubjectaltname", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/san");
-            dictionary.Add("certsubjectkeyidentifier", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/subjectkeyidentifier");
-            dictionary.Add("certsubjectname", "http://schemas.microsoft.com/2012/12/certificatecontext/field/subjectname");
-            dictionary.Add("certtemplateinformation", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/certificatetemplateinformation");
-            dictionary.Add("certtemplatename", "http://schemas.microsoft.com/2012/12/certificatecontext/extension/certificatetemplatename");
-            dictionary.Add("certthumbprint", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/thumbprint");
-            dictionary.Add("certx509version", "http://schemas.microsoft.com/2012/12/certificatecontext/field/x509version");
-            dictionary.Add("clientapplication", "http://schemas.microsoft.com/2012/01/requestcontext/claims/x-ms-client-application");
-            dictionary.Add("clientip", "http://schemas.microsoft.com/2012/01/requestcontext/claims/x-ms-client-ip");
-            dictionary.Add("clientuseragent", "http://schemas.microsoft.com/2012/01/requestcontext/claims/x-ms-client-user-agent");
-            dictionary.Add("commonname", "http://schemas.xmlsoap.org/claims/CommonName");
-            dictionary.Add("denyonlyprimarygroupsid", "http://schemas.microsoft.com/ws/2008/06/identity/claims/denyonlyprimarygroupsid");
-            dictionary.Add("denyonlyprimarysid", "http://schemas.microsoft.com/ws/2008/06/identity/claims/denyonlyprimarysid");
-            dictionary.Add("denyonlysid", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/denyonlysid");
-            dictionary.Add("devicedispname", "http://schemas.microsoft.com/2012/01/devicecontext/claims/displayname");
-            dictionary.Add("deviceid", "http://schemas.microsoft.com/2012/01/devicecontext/claims/identifier");
-            dictionary.Add("deviceismanaged", "http://schemas.microsoft.com/2012/01/devicecontext/claims/ismanaged");
-            dictionary.Add("deviceostype", "http://schemas.microsoft.com/2012/01/devicecontext/claims/ostype");
-            dictionary.Add("deviceosver", "http://schemas.microsoft.com/2012/01/devicecontext/claims/osversion");
-            dictionary.Add("deviceowner", "http://schemas.microsoft.com/2012/01/devicecontext/claims/userowner");
-            dictionary.Add("deviceregid", "http://schemas.microsoft.com/2012/01/devicecontext/claims/registrationid");
-            dictionary.Add("endpointpath", "http://schemas.microsoft.com/2012/01/requestcontext/claims/x-ms-endpoint-absolute-path");
-            dictionary.Add("forwardedclientip", "http://schemas.microsoft.com/2012/01/requestcontext/claims/x-ms-forwarded-client-ip");
-            dictionary.Add("group", "http://schemas.xmlsoap.org/claims/Group");
-            dictionary.Add("groupsid", "http://schemas.microsoft.com/ws/2008/06/identity/claims/groupsid");
-            dictionary.Add("idp", "http://schemas.microsoft.com/identity/claims/identityprovider");
-            dictionary.Add("insidecorporatenetwork", "http://schemas.microsoft.com/ws/2012/01/insidecorporatenetwork");
-            dictionary.Add("isregistereduser", "http://schemas.microsoft.com/2012/01/devicecontext/claims/isregistereduser");
-            //dictionary.Add("ppid", ClaimTypes.PPID);
-            dictionary.Add("primarygroupsid", "http://schemas.microsoft.com/ws/2008/06/identity/claims/primarygroupsid");
-            dictionary.Add("primarysid", "http://schemas.microsoft.com/ws/2008/06/identity/claims/primarysid");
-            dictionary.Add("proxy", "http://schemas.microsoft.com/2012/01/requestcontext/claims/x-ms-proxy");
-            dictionary.Add("pwdchgurl", "http://schemas.microsoft.com/ws/2012/01/passwordchangeurl");
-            dictionary.Add("pwdexpdays", "http://schemas.microsoft.com/ws/2012/01/passwordexpirationdays");
-            dictionary.Add("pwdexptime", "http://schemas.microsoft.com/ws/2012/01/passwordexpirationtime");
-            dictionary.Add("relyingpartytrustid", "http://schemas.microsoft.com/2012/01/requestcontext/claims/relyingpartytrustid");
-            dictionary.Add("role", "http://schemas.microsoft.com/ws/2008/06/identity/claims/role");
-            dictionary.Add("upn", "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/upn");
-            dictionary.Add("winaccountname", "http://schemas.microsoft.com/ws/2008/06/identity/claims/windowsaccountname");
-
-            Dictionary<string, string> dictionary2 = new Dictionary<string, string>();
-            foreach(var item in dictionary)
-            {
-                if (!dictionary2.ContainsKey(item.Value))
-                {
-                    dictionary2.Add(item.Value, item.Key);
-                }
-            }
-
-            dictionary2.Add("unique_name", "name");
-            return dictionary2;
-        }
-
-        static string MapClaimType(string type)
-        {
-            if (ClaimTypeMap.ContainsKey(type))
-            { 
-                return ClaimTypeMap[type];
-            }
-            
-            return type;
-        }
-
         protected virtual IEnumerable<Claim> NormalizeExternalClaimTypes(IEnumerable<Claim> incomingClaims)
         {
-            var claimsToMap = incomingClaims.Where(x => ClaimTypeMap.ContainsKey(x.Type));
-            var mappedClaims = claimsToMap.Select(x => new Claim(MapClaimType(x.Type), x.Value));
-
-            var claims = new List<Claim>();
-            claims.AddRange(incomingClaims.Except(claimsToMap));
-            claims.AddRange(mappedClaims);
-            
-            return claims;
+            return Thinktecture.IdentityServer.Core.Plumbing.ClaimMap.Map(incomingClaims);
         }
     }
 }
