de92157155dfa594144db79fcd49ed86b03e8e83
Dominick Baier <dbaier@leastprivilege.com>
2014-04-06 11:43:38 +0200

    Added startup_minimal

---
+++ source/Host/Startup_Minimal.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.Facebook;
+using Microsoft.Owin.Security.Google;
+using Owin;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.TestServices;
+
+namespace Thinktecture.IdentityServer.Host.Sample
+{
+    public class Startup_Minimal
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/core", coreApp =>
+                {
+                    var factory = TestOptionsFactory.Create(
+                        issuerUri:         "https://idsrv3.com",
+                        siteName:          "Thinktecture IdentityServer v3 - preview 1",
+                        certificateName:   "CN=idsrv3test",
+                        publicHostAddress: "http://localhost:3333");
+                    
+                    var opts = new IdentityServerCoreOptions
+                    {
+                        Factory = factory,
+                    };
+
+                    coreApp.UseIdentityServerCore(opts);
+                });
+        }
+    }
+}
