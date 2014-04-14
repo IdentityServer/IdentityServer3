8d466fa6ea3e789d4afc40a64528a0463b512edd
Dominick Baier <dbaier@leastprivilege.com>
2014-04-14 08:26:12 +0200

    Added logger for Diagnostics.Trace

---
+++ source/Core/Services/TraceLogger.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityServer.Core.Services
+{
+    public class TraceLogger : ILogger
+    {
+        public void Verbose(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        public void Information(string message)
+        {
+            Trace.TraceInformation(message);
+        }
+
+        public void Warning(string message)
+        {
+            Trace.TraceWarning(message);
+        }
+
+        public void Error(string message)
+        {
+            Trace.TraceError(message);
+        }
+
+        public void Start(string action)
+        {
+            Trace.TraceInformation("Start: " + action);
+        }
+    }
+}
