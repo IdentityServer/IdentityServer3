64eed2daa4ff4be767e78cfb3e592f727857af06
Dominick Baier <dbaier@leastprivilege.com>
2014-04-23 21:53:08 +0200

    Added sample ASP.NET Web API endpoint + JWT validation

--- samples/Clients/Constants/Constants.cs
+++ samples/Clients/Constants/Constants.cs
@@ -8,5 +8,7 @@
         public const string AuthorizeEndpoint = BaseAddress + "/connect/authorize";
         public const string LogoutEndpoint = BaseAddress + "/connect/logout";
         public const string TokenEndpoint = BaseAddress + "/connect/token";
+
+        public const string AspNetWebApiSampleApi = "http://localhost:2727/";
     }
 }
