d8c030a653d19099153a274ad34964476516cdb9
Dominick Baier <dbaier@leastprivilege.com>
2014-04-24 06:27:29 +0200

    Added sample how to get to access token with OWIN middleware

--- samples/Clients/MVC OWIN Client/Startup.cs
+++ samples/Clients/MVC OWIN Client/Startup.cs
@@ -1,10 +1,14 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin;
 using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Notifications;
 using Microsoft.Owin.Security.OpenIdConnect;
 using Owin;
 using Sample;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 [assembly: OwinStartup(typeof(MVC_OWIN_Client.Startup))]
 
@@ -26,10 +30,34 @@ namespace MVC_OWIN_Client
                     Client_Id = "implicitclient",
                     Authority = Constants.BaseAddress,
                     Redirect_Uri = "http://localhost:2671/",
-                    Response_Type = "id_token",
-                    Scope = "openid email",
+                    Response_Type = "id_token token",
+                    Scope = "openid email read",
 
-                    SignInAsAuthenticationType = "Cookies"
+                    SignInAsAuthenticationType = "Cookies",
+
+                    // sample how to access token on form (for token response type)
+                    Notifications = new OpenIdConnectAuthenticationNotifications
+                    {
+                        MessageReceived = async n =>
+                            {
+                                var token = n.ProtocolMessage.Token;
+
+                                if (!string.IsNullOrEmpty(token))
+                                {
+                                    n.OwinContext.Set<string>("idsrv:token", token);
+                                }
+                            },
+                        SecurityTokenValidated = async n =>
+                            {
+                                var token = n.OwinContext.Get<string>("idsrv:token");
+
+                                if (!string.IsNullOrEmpty(token))
+                                {
+                                    n.AuthenticationTicket.Identity.AddClaim(
+                                        new Claim("access_token", token));
+                                }
+                            }
+                    }
                 });
         }
     }
