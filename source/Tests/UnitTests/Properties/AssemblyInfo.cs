e1c557e75f7c76412cd19037aa20fb9440306e4b
Maurice <maurice.de.beijer@gmail.com>
2014-04-28 15:38:57 +0200

    Write VersionAssemblyInfo.cs on build

--- source/Tests/UnitTests/Properties/AssemblyInfo.cs
+++ source/Tests/UnitTests/Properties/AssemblyInfo.cs
@@ -25,15 +25,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("b806a20e-77db-4551-94cd-245039a57c98")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
