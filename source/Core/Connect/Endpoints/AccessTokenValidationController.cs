1a57b244a1bf09732214a53a214f133c2b628a99
Dominick Baier <dbaier@leastprivilege.com>
2014-04-29 21:04:12 +0200

    Prototype of access token validation endpoint

---
+++ source/Core/Connect/Endpoints/AccessTokenValidationController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Net.Http;
+
+namespace Thinktecture.IdentityServer.Core.Connect
+{
+    [RoutePrefix("connect/accessTokenValidation")]
+    public class AccessTokenValidationController : ApiController
+    {
+        private readonly TokenValidator _validator;
+
+        public AccessTokenValidationController(TokenValidator validator)
+        {
+            _validator = validator;
+        }
+
+        [Route]
+        public async Task<dynamic> Get()
+        {
+            var parameters = Request.RequestUri.ParseQueryString();
+
+            var token = parameters.Get("token");
+            if (token.IsMissing())
+            {
+                return BadRequest("token is missing.");
+            }
+
+            var result = await _validator.ValidateAccessTokenAsync(token, parameters.Get("expectedScope"));
+            
+            if (result.IsError)
+            {
+                return Unauthorized(new AuthenticationHeaderValue("Bearer", result.Error));
+            }
+
+            return result.Claims.Select(c => new { c.Type, c.Value });
+        }
+    }
+}
