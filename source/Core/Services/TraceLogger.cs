129f75a8a308af758820a2e27b945ef2a0b8a792
Dominick Baier <dbaier@leastprivilege.com>
2014-04-29 21:14:36 +0200

    Added debugger step through to TraceLogger

--- source/Core/Services/TraceLogger.cs
+++ source/Core/Services/TraceLogger.cs
@@ -9,26 +9,31 @@ namespace Thinktecture.IdentityServer.Core.Services
 {
     public class TraceLogger : ILogger
     {
+        [DebuggerStepThrough]
         public void Verbose(string message)
         {
             Trace.TraceInformation(message);
         }
 
+        [DebuggerStepThrough]
         public void Information(string message)
         {
             Trace.TraceInformation(message);
         }
 
+        [DebuggerStepThrough]
         public void Warning(string message)
         {
             Trace.TraceWarning(message);
         }
 
+        [DebuggerStepThrough]
         public void Error(string message)
         {
             Trace.TraceError(message);
         }
 
+        [DebuggerStepThrough]
         public void Start(string action)
         {
             Trace.TraceInformation("Start: " + action);
