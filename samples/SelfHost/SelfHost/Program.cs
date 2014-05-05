19fd0cd32107e9dc090bdaabe257c55dee6e7fdb
Dominick Baier <dbaier@leastprivilege.com>
2014-05-05 07:55:28 +0200

    Changed selfhost URL to /core for now to make all samples works by default

--- samples/SelfHost/SelfHost/Program.cs
+++ samples/SelfHost/SelfHost/Program.cs
@@ -10,7 +10,7 @@ namespace SelfHost
     {
         private static void Main(string[] args)
         {
-            const string url = "http://localhost:3333";
+            const string url = "http://localhost:3333/core";
             using (WebApp.Start<Startup>(url))
             {
                 Console.WriteLine("Server listening at {0}. Press a key to stop", url);
