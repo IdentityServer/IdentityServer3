755bcb39d5c7264ff33c7ab80799fffd7b779eb6
Dominick Baier <dbaier@leastprivilege.com>
2014-05-06 08:43:33 +0200

    Cleanup Client model

--- source/Core/Models/Client.cs
+++ source/Core/Models/Client.cs
@@ -15,62 +15,27 @@ namespace Thinktecture.IdentityServer.Core.Models
         public string ClientName { get; set; }
         public string ClientUri { get; set; }
         public Uri LogoUri { get; set; }
-        public ApplicationTypes ApplicationType { get; set; }
+        
         public bool RequireConsent { get; set; }
         public bool AllowRememberConsent { get; set; }
 
         public Flows Flow { get; set; }
-        //public bool AllowOpenId { get; set; }
         public List<Uri> RedirectUris { get; set; }
         
+        // in seconds
         public int IdentityTokenLifetime { get; set; }
         public int AccessTokenLifetime { get; set; }
         public int RefreshTokenLifetime { get; set; }
         public int AuthorizationCodeLifetime { get; set; }
 
         public List<string> ScopeRestrictions { get; set; }
+        public SigningKeyTypes IdentityTokenSigningKeyType { get; set; }
+        public AccessTokenType AccessTokenType { get; set; }
 
+        // not implemented yet
+        public bool RequireSignedAuthorizeRequest { get; set; }
         public SubjectTypes SubjectType { get; set; }
         public Uri SectorIdentifierUri { get; set; }
-        public SigningKeyTypes IdentityTokenSigningKeyType { get; set; }
-        
-        public bool RequireSignedAuthorizeRequest { get; set; }
-
-        // todo: makes sense? must be always signed by def. x509 cert
-        // how does userInfo endpoint distinguish?
-        public AccessTokenType AccessTokenType { get; set; }
-    }
-
-    public enum Flows
-    {
-        Code,
-        Implicit,
-        ClientCredentials,
-        ResourceOwner,
-        Assertion
-    }
-
-    public enum SubjectTypes
-    {
-        Global, 
-        PPID
-    };
-
-    public enum ApplicationTypes
-    {
-        Web, 
-        Native
-    };
-
-    public enum SigningKeyTypes
-    {
-        Default, 
-        ClientSecret
-    };
-
-    public enum AccessTokenType
-    {
-        JWT, 
-        Reference
+        public ApplicationTypes ApplicationType { get; set; }
     }
 }
---
+++ source/Core/Models/Enums.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityServer.Core.Models
+{
+    public enum Flows
+    {
+        Code,
+        Implicit,
+        ClientCredentials,
+        ResourceOwner,
+        Assertion
+    }
+
+    public enum SubjectTypes
+    {
+        Global,
+        PPID
+    };
+
+    public enum ApplicationTypes
+    {
+        Web,
+        Native
+    };
+
+    public enum SigningKeyTypes
+    {
+        Default,
+        ClientSecret
+    };
+
+    public enum AccessTokenType
+    {
+        JWT,
+        Reference
+    }
+}
