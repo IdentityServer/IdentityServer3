53e7be9b790e651ff30673f27507969269df9877
Dominick Baier <dbaier@leastprivilege.com>
2014-05-14 14:58:12 +0200

    Added sample relying party using OWIN WS-Fed MW

--- source/WsFed/Services/TestRelyingPartyService.cs
+++ source/WsFed/Services/TestRelyingPartyService.cs
@@ -6,14 +6,15 @@ using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Thinktecture.IdentityServer.WsFed.Models;
+using System.Linq;
 
 namespace Thinktecture.IdentityServer.WsFed.Services
 {
     public class TestRelyingPartyService : IRelyingPartyService
     {
-        public Task<RelyingParty> GetByRealmAsync(string realm)
+        IEnumerable<RelyingParty> _rps = new List<RelyingParty>
         {
-            return Task.FromResult(new RelyingParty
+            new RelyingParty
             {
                 Realm = "urn:testrp",
                 Enabled = true,
@@ -28,7 +29,28 @@ namespace Thinktecture.IdentityServer.WsFed.Services
                     { "given_name", ClaimTypes.GivenName },
                     { "email", ClaimTypes.Email }
                 }
-            });
+            },
+            new RelyingParty
+            {
+                Realm = "urn:owinrp",
+                Enabled = true,
+                ReplyUrl = "http://localhost:10313/",
+                TokenType = Thinktecture.IdentityModel.Constants.TokenTypes.Saml2TokenProfile11,
+                TokenLifeTime = 1,
+
+                ClaimMappings = new Dictionary<string, string>
+                {
+                    { "sub", ClaimTypes.NameIdentifier },
+                    { "name", ClaimTypes.Name },
+                    { "given_name", ClaimTypes.GivenName },
+                    { "email", ClaimTypes.Email }
+                }
+            }
+        };
+
+        public Task<RelyingParty> GetByRealmAsync(string realm)
+        {
+            return Task.FromResult(_rps.FirstOrDefault(rp => rp.Realm == realm));
         }
     }
 }
