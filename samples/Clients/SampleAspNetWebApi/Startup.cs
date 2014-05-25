6cc4dadb50094c215113760f4db3ff23b578e6a2
Dominick Baier <dbaier@leastprivilege.com>
2014-05-25 17:47:55 +0200

    Added comment to startup

--- samples/Clients/SampleAspNetWebApi/Startup.cs
+++ samples/Clients/SampleAspNetWebApi/Startup.cs
@@ -16,11 +16,13 @@ namespace SampleAspNetWebApi
         {
             JwtSecurityTokenHandler.InboundClaimTypeMap = ClaimMappings.None;
 
+            // for self contained tokens
             app.UseJsonWebToken(
                 issuer:    "https://idsrv3.com",
                 audience:  "https://idsrv3.com/resources",
                 signingKey: X509.LocalMachine.TrustedPeople.SubjectDistinguishedName.Find("CN=idsrv3test", false).First());
 
+            // for reference tokens
             app.UseIdentitiyServerReferenceTokens(new ReferenceTokenValidationOptions
                 {
                     TokenValidationEndpoint = "http://localhost:3333/core/connect/accessTokenValidation"
