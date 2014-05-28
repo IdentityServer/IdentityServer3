7b1c814258800921c62bcdde2e71f27676e0b7da
Dominick Baier <dbaier@leastprivilege.com>
2014-05-28 16:14:08 +0200

    Updated selfhost

--- samples/SelfHost/SelfHost/Program.cs
+++ samples/SelfHost/SelfHost/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using Microsoft.Owin.Hosting;
-using Owin;
-using Thinktecture.IdentityServer.Core.Configuration;
-using Thinktecture.IdentityServer.Host.Config;
+﻿using Microsoft.Owin.Hosting;
+using System;
 
 namespace SelfHost
 {
@@ -15,24 +12,6 @@ namespace SelfHost
             {
                 Console.WriteLine("Server listening at {0}. Press a key to stop", url);
                 Console.ReadKey();
-            }
-        }
-
-        internal class Startup
-        {
-            public void Configuration(IAppBuilder appBuilder)
-            {
-                var factory = LocalTestFactory.Create(
-                        issuerUri: "https://idsrv3.com",
-                        siteName: "Thinktecture IdentityServer v3 - preview 1 (SelfHost)",
-                        publicHostAddress: "http://localhost:3333");
-
-                var opts = new IdentityServerCoreOptions
-                {
-                    Factory = factory,
-                };
-
-                appBuilder.UseIdentityServerCore(opts);
             }
         }
     }
---
+++ samples/SelfHost/SelfHost/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Owin;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Thinktecture.IdentityServer.Host.Config;
+
+namespace SelfHost
+{
+    internal class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            var factory = LocalTestFactory.Create(
+                    issuerUri: "https://idsrv3.com",
+                    siteName: "Thinktecture IdentityServer v3 - preview 1 (SelfHost)",
+                    publicHostAddress: "http://localhost:3333");
+
+            var opts = new IdentityServerCoreOptions
+            {
+                Factory = factory,
+            };
+
+            appBuilder.UseIdentityServerCore(opts);
+        }
+    }
+}
