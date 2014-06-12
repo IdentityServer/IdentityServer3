d0984383db5aaa729e13ef490845b9be092cf14f
Dominick Baier <dbaier@leastprivilege.com>
2014-06-12 23:47:29 +0200

    Added Diagnostics.Trace logger

---
+++ source/Core/Logging/LogProviders/DiagnosticsTraceLogProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Thinktecture.IdentityServer.Core.Logging
+{
+    public class DiagnosticsTraceLogProvider : ILogProvider
+    {
+        public ILog GetLogger(string name)
+        {
+            return new DiagnosticsTraceLogger();
+        }
+    }
+
+    public class DiagnosticsTraceLogger : ILog
+    {
+        public void Log(LogLevel logLevel, Func<string> messageFunc)
+        {
+            Trace.WriteLine(messageFunc(), logLevel.ToString());
+        }
+
+        public void Log<TException>(LogLevel logLevel, Func<string> messageFunc, TException exception) where TException : Exception
+        {
+            Trace.WriteLine(messageFunc() + ": " + exception.ToString(), logLevel.ToString());
+        }
+    }
+}
