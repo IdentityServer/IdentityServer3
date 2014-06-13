fad11f73573befe70a8fb695468c7bf08f9263e3
Dominick Baier <dbaier@leastprivilege.com>
2014-06-13 08:42:07 +0200

    Updated logger classes

--- source/Core/Logging/LogProviders/DiagnosticsTraceLogProvider.cs
+++ source/Core/Logging/LogProviders/DiagnosticsTraceLogProvider.cs
@@ -7,20 +7,32 @@ namespace Thinktecture.IdentityServer.Core.Logging
     {
         public ILog GetLogger(string name)
         {
-            return new DiagnosticsTraceLogger();
+            return new DiagnosticsTraceLogger(name);
         }
     }
 
     public class DiagnosticsTraceLogger : ILog
     {
+        string _name;
+
+        public DiagnosticsTraceLogger()
+        {
+            _name = "";
+        }
+
+        public DiagnosticsTraceLogger(string name)
+        {
+            _name = string.Format("[{0}]: ", name);
+        }
+
         public void Log(LogLevel logLevel, Func<string> messageFunc)
         {
-            Trace.WriteLine(messageFunc(), logLevel.ToString());
+            Trace.WriteLine(_name + messageFunc(), logLevel.ToString());
         }
 
         public void Log<TException>(LogLevel logLevel, Func<string> messageFunc, TException exception) where TException : Exception
         {
-            Trace.WriteLine(messageFunc() + ": " + exception.ToString(), logLevel.ToString());
+            Trace.WriteLine(_name + messageFunc() + ": " + exception.ToString(), logLevel.ToString());
         }
     }
 }
