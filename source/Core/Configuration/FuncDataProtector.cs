be8af7d3162e168d1a60748d1727de0de78701f2
Dominick Baier <dbaier@leastprivilege.com>
2014-06-25 19:52:35 +0200

    Added FuncDataProtector

---
+++ source/Core/Configuration/FuncDataProtector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Thinktecture.IdentityServer.Core.Configuration
+{
+    public class FuncDataProtector : IDataProtector
+    {
+        Func<byte[], string, byte[]> _protectFunc;
+        Func<byte[], string, byte[]> _unprotectFunc;
+
+        public FuncDataProtector(
+            Func<byte[], string, byte[]> protectFunc,
+            Func<byte[], string, byte[]> unprotectFunc)
+        {
+            _protectFunc = protectFunc;
+            _unprotectFunc = unprotectFunc;
+        }
+
+        public byte[] Protect(byte[] data, string entropy = "")
+        {
+            return _protectFunc(data, entropy);
+        }
+
+        public byte[] Unprotect(byte[] data, string entropy = "")
+        {
+            return _unprotectFunc(data, entropy);
+        }
+    }
+}
