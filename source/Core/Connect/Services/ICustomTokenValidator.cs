c512cda3f9e5e17043ae4a02dee9b3e5622fae7a
Dominick Baier <dbaier@leastprivilege.com>
2014-07-07 07:19:21 +0200

    Added license headers

--- source/Core/Connect/Services/ICustomTokenValidator.cs
+++ source/Core/Connect/Services/ICustomTokenValidator.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+using System.Threading.Tasks;
 using Thinktecture.IdentityServer.Core.Configuration;
 using Thinktecture.IdentityServer.Core.Services;
 
