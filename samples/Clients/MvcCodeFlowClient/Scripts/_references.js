7bc789d158417c12effd7a4e720ccaa40ea8633a
Dominick Baier <dbaier@leastprivilege.com>
2014-07-17 09:54:51 +0200

    moves samples

--- samples/Clients/MvcCodeFlowClient/Scripts/_references.js
+++
@@ -1,6 +0,0 @@
-﻿/// <reference path="modernizr-2.7.2.js" />
-/// <reference path="jquery-ui-1.10.4.js" />
-/// <reference path="jquery-2.1.0.js" />
-/// <reference path="jquery.validate.js" />
-/// <reference path="jquery.validate.unobtrusive.js" />
-/// <reference path="knockout-2.2.0.debug.js" />
--- samples/Clients/MvcCodeFlowClientManual/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MvcCodeFlowClientManual.MvcApplication" Language="C#" %>
