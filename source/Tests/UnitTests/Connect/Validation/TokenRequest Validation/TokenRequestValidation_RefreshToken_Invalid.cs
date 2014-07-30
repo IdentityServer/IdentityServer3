78576c463271c2bcbf5e50032559badc275811cc
Dominick Baier <dbaier@leastprivilege.com>
2014-07-30 07:43:43 +0200

    Added refresh token validation unit tests

---
+++ source/Tests/UnitTests/Connect/Validation/TokenRequest Validation/TokenRequestValidation_RefreshToken_Invalid.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * Copyright (c) Dominick Baier, Brock Allen.  All rights reserved.
+ * see license
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Specialized;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Core;
+using Thinktecture.IdentityServer.Core.Connect.Models;
+using Thinktecture.IdentityServer.Core.Services;
+using Thinktecture.IdentityServer.Core.Services.InMemory;
+using UnitTests.Plumbing;
+
+namespace UnitTests.Validation_Tests.TokenRequest_Validation
+{
+    [TestClass]
+    public class TokenRequestValidation_RefreshToken_Invalid
+    {
+        const string Category = "TokenRequest Validation - RefreshToken - Invalid";
+
+        IClientStore _clients = Factory.CreateClientStore();
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Non_existing_RefreshToken()
+        {
+            var store = new InMemoryRefreshTokenStore();
+            var client = await _clients.FindClientByIdAsync("roclient");
+
+            var validator = Factory.CreateTokenValidator(
+                refreshTokens: store);
+
+            var parameters = new NameValueCollection();
+            parameters.Add(Constants.TokenRequest.GrantType, "refresh_token");
+            parameters.Add(Constants.TokenRequest.RefreshToken, "nonexistent");
+
+            var result = await validator.ValidateRequestAsync(parameters, client);
+
+            Assert.IsTrue(result.IsError);
+            Assert.AreEqual(Constants.TokenErrors.InvalidGrant, result.Error);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Expired_RefreshToken()
+        {
+            var refreshToken = new RefreshToken
+            {
+                AccessToken = new Token("access_token"),
+                ClientId = "roclient",
+                LifeTime = 10,
+                Handle = Guid.NewGuid().ToString(),
+                CreationTime = DateTime.UtcNow.AddSeconds(-15)
+            };
+
+            var store = new InMemoryRefreshTokenStore();
+            await store.StoreAsync(refreshToken.Handle, refreshToken);
+
+            var client = await _clients.FindClientByIdAsync("roclient");
+
+            var validator = Factory.CreateTokenValidator(
+                refreshTokens: store);
+
+            var parameters = new NameValueCollection();
+            parameters.Add(Constants.TokenRequest.GrantType, "refresh_token");
+            parameters.Add(Constants.TokenRequest.RefreshToken, refreshToken.Handle);
+
+            var result = await validator.ValidateRequestAsync(parameters, client);
+
+            Assert.IsTrue(result.IsError);
+            Assert.AreEqual(Constants.TokenErrors.InvalidGrant, result.Error);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Wrong_Client_Binding_RefreshToken_Request()
+        {
+            var refreshToken = new RefreshToken
+            {
+                AccessToken = new Token("access_token"),
+                ClientId = "otherclient",
+                LifeTime = 600,
+                Handle = Guid.NewGuid().ToString(),
+                CreationTime = DateTime.UtcNow
+            };
+
+            var store = new InMemoryRefreshTokenStore();
+            await store.StoreAsync(refreshToken.Handle, refreshToken);
+
+            var client = await _clients.FindClientByIdAsync("roclient");
+
+            var validator = Factory.CreateTokenValidator(
+                refreshTokens: store);
+
+            var parameters = new NameValueCollection();
+            parameters.Add(Constants.TokenRequest.GrantType, "refresh_token");
+            parameters.Add(Constants.TokenRequest.RefreshToken, refreshToken.Handle);
+
+            var result = await validator.ValidateRequestAsync(parameters, client);
+
+            Assert.IsTrue(result.IsError);
+            Assert.AreEqual(Constants.TokenErrors.InvalidGrant, result.Error);
+        }
+
+        [TestMethod]
+        [TestCategory(Category)]
+        public async Task Client_has_no_OfflineAccess_Scope_anymore_at_RefreshToken_Request()
+        {
+            var refreshToken = new RefreshToken
+            {
+                AccessToken = new Token("access_token"),
+                ClientId = "roclient_restricted",
+                LifeTime = 600,
+                Handle = Guid.NewGuid().ToString(),
+                CreationTime = DateTime.UtcNow
+            };
+
+            var store = new InMemoryRefreshTokenStore();
+            await store.StoreAsync(refreshToken.Handle, refreshToken);
+
+            var client = await _clients.FindClientByIdAsync("roclient_restricted");
+
+            var validator = Factory.CreateTokenValidator(
+                refreshTokens: store);
+
+            var parameters = new NameValueCollection();
+            parameters.Add(Constants.TokenRequest.GrantType, "refresh_token");
+            parameters.Add(Constants.TokenRequest.RefreshToken, refreshToken.Handle);
+
+            var result = await validator.ValidateRequestAsync(parameters, client);
+
+            Assert.IsTrue(result.IsError);
+            Assert.AreEqual(Constants.TokenErrors.InvalidGrant, result.Error);
+        }
+    }
+}
