fed70c505e92865256c10db71538917fb6d89015
Dominick Baier <dbaier@leastprivilege.com>
2014-09-25 17:09:24 +0200

    Added RequireSsl middleware

---
+++ source/Core/Hosting/RequireSslMiddleware.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Owin;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityServer.Core.Hosting
+{
+    public class RequireSslMiddleware
+    {
+        readonly Func<IDictionary<string, object>, Task> _next;
+
+        public RequireSslMiddleware(Func<IDictionary<string, object>, Task> next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(IDictionary<string, object> env)
+        {
+            var context = new OwinContext(env);
+
+            if (context.Request.Uri.Scheme != Uri.UriSchemeHttps)
+            {
+                context.Response.StatusCode = 403;
+                context.Response.ReasonPhrase = "SSL is required.";
+
+                return;
+            }
+
+            await _next(env);
+        }
+    }
+}
