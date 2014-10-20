2602538e95612560fa920538a8cb63dd50fa3901
Dominick Baier <dbaier@leastprivilege.com>
2014-10-20 09:29:53 +0200

    Added comments to ScopeClaim

--- source/Core/Models/ScopeClaim.cs
+++ source/Core/Models/ScopeClaim.cs
@@ -16,15 +16,37 @@
 
 namespace Thinktecture.IdentityServer.Core.Models
 {
+    /// <summary>
+    /// Models a claim that should be emitted in a token
+    /// </summary>
     public class ScopeClaim
     {
+        /// <summary>
+        /// Name of the claim
+        /// </summary>
         public string Name { get; set; }
+        
+        /// <summary>
+        /// Description of the claim
+        /// </summary>
         public string Description { get; set; }
+
+        /// <summary>
+        /// Specifies whether this claim should always be present in the identity token (even if an access token has been requested as well). Applies to identity scopes only.
+        /// </summary>
         public bool AlwaysIncludeInIdToken { get; set; }
 
+        /// <summary>
+        /// Creates an empty ScopeClaim
+        /// </summary>
         public ScopeClaim()
         { }
 
+        /// <summary>
+        /// Creates a ScopeClaim with parameters
+        /// </summary>
+        /// <param name="name">Name of the claim</param>
+        /// <param name="alwaysInclude">Specifies whether this claim should always be present in the identity token (even if an access token has been requested as well). Applies to identity scopes only.</param>
         public ScopeClaim(string name, bool alwaysInclude = false)
         {
             Name = name;
