3a252e3a42e063a254683e5f7a4be62123f4a4e5
Brock Allen <brockallen@gmail.com>
2014-11-08 15:46:16 -0500

    add claims filter to ignore incoming claims

---
+++ source/Core/Services/ExternalClaimsFilter/IgnoreClaimsFilter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thinktecture.IdentityServer.Core.Services.Default
+{
+    public class IgnoreClaimsFilter : IExternalClaimsFilter
+    {
+        readonly string[] claimTypesToIgnore;
+
+        public IgnoreClaimsFilter(params string[] claimTypesToIgnore)
+        {
+            this.claimTypesToIgnore = claimTypesToIgnore;
+        }
+        
+        public IEnumerable<Claim> Filter(string provider, IEnumerable<Claim> claims)
+        {
+            return claims.Where(x => !claimTypesToIgnore.Contains(x.Type));
+        }
+    }
+}
