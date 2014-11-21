6336c2f9049b6a91a266d6bdd7c2cd98545a376e
Damian Hickey <dhickey@gmail.com>
2014-11-21 16:29:52 +0200

    Revert a removal of a directive that resharper shouldn't have removed.

--- source/Tests/UnitTests/IdSvrHostTestBase.cs
+++ source/Tests/UnitTests/IdSvrHostTestBase.cs
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+using System.Linq;
 using Microsoft.Owin;
 using Microsoft.Owin.Testing;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
