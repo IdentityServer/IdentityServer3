068093d6bc4d722230f2f60baa0686989b6d5e89
Dominick Baier <dbaier@leastprivilege.com>
2014-11-26 16:45:27 +0100

    added prototype of redirect URI validator

---
+++ source/Core/Services/Default/DefaultRedirectUriValidator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thinktecture.IdentityServer.Core.Models;
+
+namespace Thinktecture.IdentityServer.Core.Services.Default
+{
+    public class DefaultRedirectUriValidator : IRedirectUriValidator
+    {
+        public Task<bool> IsRedirecUriValidAsync(Uri requestedUri, Client client)
+        {
+            var result = client.RedirectUris.Contains(requestedUri);
+
+            return Task.FromResult(result);
+        }
+
+        public Task<bool> IsPostLogoutRedirecUriValidAsync(Uri requestedUri, Client client)
+        {
+            var result = client.PostLogoutRedirectUris.Contains(requestedUri);
+
+            return Task.FromResult(result);
+        }
+    }
+}
