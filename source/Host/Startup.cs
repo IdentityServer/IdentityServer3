08e56a1264571c73b7e4e0bbc4d318925553ca30
Dominick Baier <dbaier@leastprivilege.com>
2014-12-26 10:35:48 +0100

    updated nugets

--- source/Host/Startup.cs
+++ source/Host/Startup.cs
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 using Microsoft.Owin;
 using Microsoft.Owin.Security.Facebook;
 using Microsoft.Owin.Security.Google;
