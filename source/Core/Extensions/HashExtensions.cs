ab58c63caff11bdc9b6437c58e10a3e46369f8ed
Dominick Baier <dbaier@leastprivilege.com>
2015-01-05 17:08:27 +0100

    Added a SHA512 helper as well

--- source/Core/Extensions/HashExtensions.cs
+++ source/Core/Extensions/HashExtensions.cs
@@ -43,5 +43,23 @@ namespace Thinktecture.IdentityServer.Core.Models
                 return Convert.ToBase64String(hash);
             }
         }
+
+        /// <summary>
+        /// Creates a SHA512 hash of the specified input.
+        /// </summary>
+        /// <param name="input">The input.</param>
+        /// <returns>A hash</returns>
+        public static string Sha512(this string input)
+        {
+            if (input.IsMissing()) return string.Empty;
+
+            using (var sha = SHA512.Create())
+            {
+                var bytes = Encoding.UTF8.GetBytes(input);
+                var hash = sha.ComputeHash(bytes);
+
+                return Convert.ToBase64String(hash);
+            }
+        }
     }
 }
