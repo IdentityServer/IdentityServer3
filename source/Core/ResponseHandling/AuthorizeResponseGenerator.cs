b7c6aff86765e66982c482d4bfb6957c6b7971e2
Dominick Baier <dbaier@leastprivilege.com>
2015-01-06 16:27:01 +0100

    updated license header

--- source/Core/ResponseHandling/AuthorizeResponseGenerator.cs
+++ source/Core/ResponseHandling/AuthorizeResponseGenerator.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright 2014 Dominick Baier, Brock Allen
+ * Copyright 2014, 2015 Dominick Baier, Brock Allen
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
