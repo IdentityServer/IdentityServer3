8a137522e521789474ab2399a61a09ceb77c77a0
Dominick Baier <dbaier@leastprivilege.com>
2015-01-24 15:02:00 +0100

    Added more cert checks at startup (#813)

--- source/Core/Configuration/AppBuilderExtensions/UseIdentityServerExtension.cs
+++ source/Core/Configuration/AppBuilderExtensions/UseIdentityServerExtension.cs
@@ -111,10 +111,17 @@ namespace Owin
 
                 return;
             }
-            if (!cert.IsPrivateAccessAllowed())
+            if (!cert.HasPrivateKey || !cert.IsPrivateAccessAllowed())
             {
-                Logger.Error("Signing certificate private key is not accessible. Make sure the account running your application has access to the private key");
+                Logger.Error("Signing certificate has not private key or private key is not accessible. Make sure the account running your application has access to the private key");
                 eventSvc.RaiseCertificatePrivateKeyNotAccessibleEvent(cert);
+
+                return;
+            }
+            if (cert.PublicKey.Key.KeySize < 2048)
+            {
+                Logger.Error("Signing certificate key length is less than 2048 bits.");
+                eventSvc.RaiseCertificateKeyLengthTooShortEvent(cert);
 
                 return;
             }
