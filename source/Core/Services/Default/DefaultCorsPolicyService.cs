3146f07c17449ac1ea0a63ba8f4b3f14c357935b
Dominick Baier <dbaier@leastprivilege.com>
2015-02-27 20:21:52 +0100

    cleanup

--- source/Core/Services/Default/DefaultCorsPolicyService.cs
+++ source/Core/Services/Default/DefaultCorsPolicyService.cs
@@ -16,7 +16,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Thinktecture.IdentityServer.Core.Configuration;
 
--- source/Core/Services/InMemory/InMemoryCorsPolicyService.cs
+++ source/Core/Services/InMemory/InMemoryCorsPolicyService.cs
@@ -16,11 +16,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Thinktecture.IdentityServer.Core.Extensions;
 using Thinktecture.IdentityServer.Core.Models;
-using Thinktecture.IdentityServer.Core.Services.Default;
 
 namespace Thinktecture.IdentityServer.Core.Services.InMemory
 {
