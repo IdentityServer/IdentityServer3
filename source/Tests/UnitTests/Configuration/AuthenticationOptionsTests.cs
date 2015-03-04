5beaf5a47c025a47816658e7f3f68967a054297c
Loc Tan Vo <loc.tan.vo@gmail.com>
2015-03-04 20:05:04 +0100

    Added SignInMessageThreshold to AuthenticationOptions with default value

---
+++ source/Tests/UnitTests/Configuration/AuthenticationOptionsTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Thinktecture.IdentityServer.Core;
+using Thinktecture.IdentityServer.Core.Configuration;
+using Xunit;
+
+namespace Thinktecture.IdentityServer.Tests.Configuration
+{
+    public class AuthenticationOptionsTests
+    {
+        [Fact]
+        public void SigninMessageThreshold_Default_SameAsDefinedConstant()
+        {
+            new AuthenticationOptions()
+                .SignInMessageThreshold
+                .Should()
+                .Be(Constants.SignInMessageThreshold);
+        }
+    }
+}
