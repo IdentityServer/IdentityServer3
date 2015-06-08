a86ef01ccf6005a50bc4feef413a934a555fe904
Brock Allen <brockallen@gmail.com>
2015-06-08 15:22:15 -0400

    license comment cleanup

--- source/Core/Services/Default/UserServiceBase.cs
+++ source/Core/Services/Default/UserServiceBase.cs
@@ -13,22 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 using IdentityServer3.Core.Models;
-/*
- * Copyright 2014, 2015 Dominick Baier, Brock Allen
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -101,7 +87,7 @@ namespace IdentityServer3.Core.Services.Default
         /// </summary>
         /// <param name="context">The context.</param>
         /// <returns></returns>
-        public virtual Task IsActiveAsync(Models.IsActiveContext context)
+        public virtual Task IsActiveAsync(IsActiveContext context)
         {
             return Task.FromResult(0);
         }
