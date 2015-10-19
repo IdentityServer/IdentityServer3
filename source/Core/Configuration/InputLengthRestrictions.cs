d37d7abcd6cbb3ab6bb140f76e630a7132034cbe
Dominick Baier <dbaier@leastprivilege.com>
2015-10-19 11:38:38 +0200

    Added input length restriction for client secret

--- source/Core/Configuration/InputLengthRestrictions.cs
+++ source/Core/Configuration/InputLengthRestrictions.cs
@@ -22,13 +22,14 @@ namespace IdentityServer3.Core.Configuration
     public class InputLengthRestrictions
     {
         private const int Default = 100;
-        
+
         /// <summary>
         /// Creates InputLengthRestrictions with default values
         /// </summary>
         public InputLengthRestrictions()
         {
             ClientId = Default;
+            ClientSecret = Default;
             Scope = 300;
             RedirectUri = 400;
             Nonce = 300;
@@ -47,6 +48,11 @@ namespace IdentityServer3.Core.Configuration
         /// Max length for client_id
         /// </summary>
         public int ClientId { get; set; }
+
+        /// <summary>
+        /// Max length for external client secrets
+        /// </summary>
+        public int ClientSecret { get; private set; }
 
         /// <summary>
         /// Max length for scope
