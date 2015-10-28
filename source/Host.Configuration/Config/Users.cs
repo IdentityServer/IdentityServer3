08b577ed301ec30fd0e11034c6569b7fec213d2b
Dominick Baier <dbaier@leastprivilege.com>
2015-10-28 18:49:51 +0100

    updated address claim

--- source/Host.Configuration/Config/Users.cs
+++ source/Host.Configuration/Config/Users.cs
@@ -37,7 +37,7 @@ namespace IdentityServer3.Host.Config
                         new Claim(Constants.ClaimTypes.Role, "Admin"),
                         new Claim(Constants.ClaimTypes.Role, "Geek"),
                         new Claim(Constants.ClaimTypes.WebSite, "http://alice.com"),
-                        new Claim(Constants.ClaimTypes.Address, "{ \"street_address\": \"One Hacker Way\", \"locality\": \"Heidelberg\", \"postal_code\": 69118, \"country\": \"Germany\" }")
+                        new Claim(Constants.ClaimTypes.Address, @"{ ""street_address"": ""One Hacker Way"", ""locality"": ""Heidelberg"", ""postal_code"": 69118, ""country"": ""Germany"" }", Constants.ClaimValueTypes.JsonObject)
                     }
                 },
                 new InMemoryUser{Subject = "88421113", Username = "bob", Password = "bob", 
@@ -51,7 +51,7 @@ namespace IdentityServer3.Host.Config
                         new Claim(Constants.ClaimTypes.Role, "Developer"),
                         new Claim(Constants.ClaimTypes.Role, "Geek"),
                         new Claim(Constants.ClaimTypes.WebSite, "http://bob.com"),
-                        new Claim(Constants.ClaimTypes.Address, "{ \"street_address\": \"One Hacker Way\", \"locality\": \"Heidelberg\", \"postal_code\": 69118, \"country\": \"Germany\" }")
+                        new Claim(Constants.ClaimTypes.Address, @"{ ""street_address"": ""One Hacker Way"", ""locality"": ""Heidelberg"", ""postal_code"": 69118, ""country"": ""Germany"" }", Constants.ClaimValueTypes.JsonObject)
                     }
                 },
             };
