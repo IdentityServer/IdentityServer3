8e8bbb075b78f79e88badcaef907af7af987efe4
Dominick Baier <dbaier@leastprivilege.com>
2015-11-17 23:33:41 +0100

    added tests for introspection endpoint

---
+++ source/Tests/UnitTests/Endpoints/Connect/Introspection/Setup/Clients.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer3.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Tests.Endpoints.Connect.Introspection.Setup
+{
+    class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client1",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    Flow = Flows.ClientCredentials,
+                    AllowAccessToAllScopes = true,
+                    AccessTokenType = AccessTokenType.Reference
+                },
+                new Client
+                {
+                    ClientId = "client2",
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    Flow = Flows.ClientCredentials,
+                    AllowAccessToAllScopes = true,
+                    AccessTokenType = AccessTokenType.Reference
+                }
+            };
+        }
+    }
+}
---
+++ source/Tests/UnitTests/Endpoints/Connect/Introspection/Setup/IntrospectionIdentityServer.cs
@@ -0,0 +1,30 @@
+﻿using IdentityServer3.Core.Configuration;
+using IdentityServer3.Core.Services.InMemory;
+using Microsoft.Owin.Builder;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Tests.Endpoints.Connect.Introspection.Setup
+{
+    class IntrospectionIdentityServer
+    {
+        public static AppBuilder Create()
+        {
+            AppBuilder app = new AppBuilder();
+
+            app.UseIdentityServer(new IdentityServerOptions
+            {
+                Factory = new IdentityServerServiceFactory()
+                            .UseInMemoryClients(Clients.Get())
+                            .UseInMemoryScopes(Scopes.Get())
+                            .UseInMemoryUsers(new List<InMemoryUser>())
+            });
+
+            return app;
+        }
+    }
+}
