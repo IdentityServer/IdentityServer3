e21f99acd9c991df09e89eeee6eaf49e2d2ef0d6
Dominick Baier <dbaier@leastprivilege.com>
2015-12-16 08:24:26 +0100

    Added generic secret parser and validator

---
+++ source/Core/Validation/SecretParser.cs
@@ -0,0 +1,40 @@
+﻿using IdentityServer3.Core.Logging;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Core.Validation
+{
+    internal class SecretParser
+    {
+        private readonly static ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        private readonly IEnumerable<ISecretParser> _parsers;
+
+        public SecretParser(IEnumerable<ISecretParser> parsers)
+        {
+            _parsers = parsers;
+        }
+
+        public async Task<ParsedSecret> ParseAsync(IDictionary<string, object> environment)
+        {
+            // see if a registered parser finds a secret on the request
+            ParsedSecret parsedSecret = null;
+            foreach (var parser in _parsers)
+            {
+                parsedSecret = await parser.ParseAsync(environment);
+                if (parsedSecret != null)
+                {
+                    Logger.DebugFormat("Parser found secret: {0}", parser.GetType().Name);
+                    Logger.InfoFormat("Secret id found: {0}", parsedSecret.Id);
+
+                    return parsedSecret;
+                }
+            }
+
+            Logger.InfoFormat("Parser found no secret");
+            return null;
+        }
+    }
+}
---
+++ source/Core/Validation/SecretValidator.cs
@@ -0,0 +1,41 @@
+﻿using IdentityServer3.Core.Logging;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer3.Core.Validation
+{
+    internal class SecretValidator
+    {
+        private readonly static ILog Logger = LogProvider.GetCurrentClassLogger();
+
+        private readonly IEnumerable<ISecretValidator> _validators;
+
+        public SecretValidator(IEnumerable<ISecretValidator> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<SecretValidationResult> ValidateAsync(ParsedSecret parsedSecret, IEnumerable<Secret> secrets)
+        {
+            // see if a registered validator can validate the secret
+            foreach (var validator in _validators)
+            {
+                var secretValidationResult = await validator.ValidateAsync(secrets, parsedSecret);
+
+                if (secretValidationResult.Success)
+                {
+                    Logger.DebugFormat("Secret validator success: {0}", validator.GetType().Name);
+                    return secretValidationResult;
+                }
+            }
+
+            Logger.Info("Secret validators could not validate secret");
+            return new SecretValidationResult { Success = false };
+        }
+    }
+}
