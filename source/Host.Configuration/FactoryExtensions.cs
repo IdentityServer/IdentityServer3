30d7ac206e7828c3ae0332c48b3b28ad512081f1
Dominick Baier <dbaier@leastprivilege.com>
2015-12-20 10:45:15 +0100

    moved custom grant registration to helper

---
+++ source/Host.Configuration/FactoryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using IdentityServer3.Core.Services;
+using IdentityServer3.Host.Config;
+
+namespace IdentityServer3.Core.Configuration
+{
+    static class FactoryExtensions
+    {
+        public static IdentityServerServiceFactory AddCustomGrantValidators(this IdentityServerServiceFactory factory)
+        {
+            factory.CustomGrantValidators.Add(
+                    new Registration<ICustomGrantValidator>(typeof(CustomGrantValidator)));
+            factory.CustomGrantValidators.Add(
+                new Registration<ICustomGrantValidator>(typeof(AnotherCustomGrantValidator)));
+
+            return factory;
+        }
+    }
+}
