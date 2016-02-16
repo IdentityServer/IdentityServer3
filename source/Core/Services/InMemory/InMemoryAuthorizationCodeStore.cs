0a266b58d287bc9b1cddaa326438398450b973df
Brock Allen <brockallen@gmail.com>
2016-02-16 13:43:56 -0500

    fix broken async helpers for in-mem stores

--- source/Core/Services/InMemory/InMemoryAuthorizationCodeStore.cs
+++ source/Core/Services/InMemory/InMemoryAuthorizationCodeStore.cs
@@ -95,7 +95,7 @@ namespace IdentityServer3.Core.Services.InMemory
         /// <param name="subject">The subject.</param>
         /// <param name="client">The client.</param>
         /// <returns></returns>
-        public Task RevokeAsync(string subject, string client)
+        public async Task RevokeAsync(string subject, string client)
         {
             var query =
                 from item in _repository
@@ -104,10 +104,8 @@ namespace IdentityServer3.Core.Services.InMemory
 
             foreach (var key in query)
             {
-                RemoveAsync(key);
+                await RemoveAsync(key);
             }
-
-            return Task.FromResult(0);
         }
     }
 }
--- source/Core/Services/InMemory/InMemoryTokenHandleStore.cs
+++ source/Core/Services/InMemory/InMemoryTokenHandleStore.cs
@@ -94,7 +94,7 @@ namespace IdentityServer3.Core.Services.InMemory
         /// <param name="subject">The subject.</param>
         /// <param name="client">The client.</param>
         /// <returns></returns>
-        public Task RevokeAsync(string subject, string client)
+        public async Task RevokeAsync(string subject, string client)
         {
             var query =
                 from item in _repository
@@ -103,10 +103,8 @@ namespace IdentityServer3.Core.Services.InMemory
 
             foreach (var key in query)
             {
-                RemoveAsync(key);
+                await RemoveAsync(key);
             }
-
-            return Task.FromResult(0);
         }
     }
 }
