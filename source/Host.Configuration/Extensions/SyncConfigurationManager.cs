951f0fee86f2d39ad2f56ce7002c69f6fe0d28b5
Dominick Baier <dbaier@leastprivilege.com>
2016-06-21 17:05:55 +0200

    add workaround for ws-fed deadlock issue

---
+++ source/Host.Configuration/Extensions/SyncConfigurationManager.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Globalization;
+
+namespace Host.Configuration.Extensions
+{
+    class SyncConfigurationManager : IConfigurationManager<WsFederationConfiguration>
+    {
+        private readonly IConfigurationManager<WsFederationConfiguration> _inner;
+
+        public SyncConfigurationManager(IConfigurationManager<WsFederationConfiguration> inner)
+        {
+            _inner = inner;
+        }
+
+        public Task<WsFederationConfiguration> GetConfigurationAsync(CancellationToken cancel)
+        {
+            var res = AsyncHelper.RunSync(() => _inner.GetConfigurationAsync(cancel));
+            return Task.FromResult(res);
+        }
+
+        public void RequestRefresh()
+        {
+            _inner.RequestRefresh();
+        }
+
+        private static class AsyncHelper
+        {
+            private static readonly TaskFactory _myTaskFactory = new TaskFactory(CancellationToken.None,
+                TaskCreationOptions.None, TaskContinuationOptions.None, TaskScheduler.Default);
+
+            public static TResult RunSync<TResult>(Func<Task<TResult>> func)
+            {
+                var cultureUi = CultureInfo.CurrentUICulture;
+                var culture = CultureInfo.CurrentCulture;
+                return _myTaskFactory.StartNew(() =>
+                {
+                    Thread.CurrentThread.CurrentCulture = culture;
+                    Thread.CurrentThread.CurrentUICulture = cultureUi;
+                    return func();
+                }).Unwrap().GetAwaiter().GetResult();
+            }
+
+            public static void RunSync(Func<Task> func)
+            {
+                var cultureUi = CultureInfo.CurrentUICulture;
+                var culture = CultureInfo.CurrentCulture;
+                _myTaskFactory.StartNew(() =>
+                {
+                    Thread.CurrentThread.CurrentCulture = culture;
+                    Thread.CurrentThread.CurrentUICulture = cultureUi;
+                    return func();
+                }).Unwrap().GetAwaiter().GetResult();
+            }
+        }
+    }
+}
